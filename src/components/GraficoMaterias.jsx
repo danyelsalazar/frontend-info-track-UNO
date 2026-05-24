07ebae4e614cd5db0058349429df19091c9f1c43
danyel salazar <danygasia2@gmail.com>
2026-05-23 21:10:07 -0300

    style: dashboard

--- src/components/GraficoMaterias.jsx
+++ src/components/GraficoMaterias.jsx
@@ -1,12 +1,5 @@
 import { PieChart } from "@mui/x-charts";
 
-const data = [
-  { label: 'Group A', value: 30, color: '#d62c5b' },
-  { label: 'Group B', value: 40, color: '#144a75' },
-  { label: 'Group C', value: 60, color: '#2fa4a9' },
-  { label: 'Group D', value: 32, color: '#009cd1' },
-];
-
 const settings = {
   margin: { right: 5 },
   width: 200,
@@ -14,7 +7,7 @@ const settings = {
   hideLegend: true,
 };
 
-export default function GraficoMaterias() {
+export default function GraficoMaterias({data}) {
   return (
     <PieChart
       series={[{ innerRadius: 30, outerRadius: 80, data, arcLabel: 'value' }]}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -19,11 +19,17 @@ const TAREAS = [
 const ACCESOS = [
   { label: "Plan de estudios", icon: <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><g fill="none"><path d="M20 22V4h-4v2H8V4H4v18z"/><path d="M16 6H8V2h8z"/><path stroke="#666666" strokeWidth="2" d="M16 4h4v18H4V4h4m8 0V2H8v2m8 0v2H8V4"/><path stroke="#666666" strokeLinecap="square" strokeWidth="2" d="M10 12h4m-4 4h4"/></g></svg> },
   { label: "Calendario",       icon: <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><path fill="var(--color-primary)" d="M19 19H5V8h14m-3-7v2H8V1H6v2H5c-1.11 0-2 .89-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2h-1V1m-1 11h-5v5h5z"/></svg> },
-  { label: "Reseñas",          icon: <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><path fill="none" stroke="#666666" stroke-linejoin="round" stroke-width="2" d="m12 2l3.104 6.728l7.358.873l-5.44 5.03l1.444 7.268L12 18.28L5.534 21.9l1.444-7.268L1.538 9.6l7.359-.873z"/></svg>},
+  { label: "Reseñas",          icon: <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><path fill="none" stroke="#666666" strokeLinejoin="round" strokeWidth="2" d="m12 2l3.104 6.728l7.358.873l-5.44 5.03l1.444 7.268L12 18.28L5.534 21.9l1.444-7.268L1.538 9.6l7.359-.873z"/></svg>},
   { label: "Recursos",         icon: <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 20 20"><path fill="#666666" d="M18.405 4.799c-.111-.44-.655-.799-1.21-.799h-6.814c-.554 0-1.33-.318-1.722-.707l-.596-.588C7.671 2.316 6.896 2 6.342 2H3.087c-.555 0-1.059.447-1.12.994L1.675 6h16.931zM19.412 7H.588a.58.58 0 0 0-.577.635l.923 9.669A.77.77 0 0 0 1.7 18h16.6a.77.77 0 0 0 .766-.696l.923-9.669A.58.58 0 0 0 19.412 7"/></svg> },
-  { label: "Mi perfil",        icon: <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><g fill="none" stroke="var(--color-primary)" stroke-width="2"><path stroke-linejoin="round" d="M4 18a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2Z"/><circle cx="12" cy="7" r="3"/></g></svg> },
+  { label: "Mi perfil",        icon: <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><g fill="none" stroke="var(--color-primary)" strokeWidth="2"><path strokeLinejoin="round" d="M4 18a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2Z"/><circle cx="12" cy="7" r="3"/></g></svg> },
   { label: "Materias",        icon: <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><g fill="none"><path d="M20.5 2.5h-14a3 3 0 0 0-3 3v13a3 3 0 0 1 3-3h14z"/><path stroke="#666666" strokeLinecap="square" strokeWidth="2" d="M20.5 21.5h-14a3 3 0 1 1 0-6h14zm0 0v-19h-14a3 3 0 0 0-3 3v12m8-11h5"/></g></svg>}
 
+];
+
+const dataMock = [
+  { label: 'Aprobadas', value: STATS[0].value, color: 'var(--color-accent)' },
+  { label: 'Cursando', value: STATS[1].value, color: 'var(--color-primary)' },
+  { label: 'Restantes', value: STATS[3].value , color: 'var(--color-dark)' },
 ];
 
 
@@ -67,7 +73,7 @@ export default function Dashboard() {
             </div>
           ))}
         </div>
-          <GraficoMaterias/>
+          <GraficoMaterias data={dataMock}/>
         </div>
         {/* TAREAS */}
         <div className="tareas-section">
