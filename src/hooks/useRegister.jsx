2f8044dc2a0a89c12513dec0f6113c0021b49895
danyel salazar <danygasia2@gmail.com>
2026-05-28 20:55:50 -0300

    feature: alert in the register

--- src/hooks/useRegister.jsx
+++ src/hooks/useRegister.jsx
@@ -1,15 +1,17 @@
-import { useMutation } from "@apollo/client/react"
-import { REGISTER } from "../graphql/usuario.mutations"
-import { useNavigate } from "react-router-dom"
-import { useState } from "react"
+import { useMutation } from "@apollo/client/react";
+import { REGISTER } from "../graphql/usuario.mutations";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+// para la alerte de registro
+const MySwal = withReactContent(Swal);
 
 export const useRegister = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  // Una vez registrado el usuario mandarlo al login
-  const [registrarUsuario ,{loading, error}] = useMutation(REGISTER, {
-    onCompleted: () => navigate('/login')
-  })
+  const [registrarUsuario, { loading, error }] = useMutation(REGISTER);
 
   const [form, setForm] = useState({
     nombre: "",
@@ -26,10 +28,33 @@ export const useRegister = () => {
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    registrarUsuario({variables: form})
+
+    try {
+      await registrarUsuario({ variables: form });
+      // si todo sale bien con el registro avisamos que se registro
+      await MySwal.fire({
+        title: "¡Registro exitoso!",
+        text: "Tu cuenta fue creada correctamente",
+        icon: "success",
+        confirmButtonText: "Continuar",
+        width: "350px",
+        confirmButtonColor: "#d62c5b", // color botón
+      });
+      // presiona aceptar y lo mandamos al loguin
+      navigate("/login");
+    } catch (err) {
+      // si hay algun error con el registyro le mostramos que no se registro
+      MySwal.fire({
+        title: "Error",
+        text: err.message,
+        icon: "error",
+        width: "350px",
+        confirmButtonColor: "#d62c5b",
+      });
+    }
   };
 
-  return {loading, error, form, navigate, handleChange, handleSubmit}
-}
+  return { loading, error, form, navigate, handleChange, handleSubmit };
+};
