ebf46b63b3e3973dfbdde823c4c32199d57eff5b
Ezagu <agustincalpedm@gmail.com>
2026-05-31 04:34:39 -0300

    modificacion de indicar estado en materia

--- src/pages/Materia.jsx
+++ src/pages/Materia.jsx
@@ -1,16 +1,15 @@
-import { IconBrandWhatsapp, IconCalendarCheck, IconStar } from "@tabler/icons-react";
+import { IconBrandWhatsapp, IconCalendarCheck, IconCirclePlus, IconStar } from "@tabler/icons-react";
 import { useMateria } from "../hooks/useMateria"
 import { MateriaBadge } from "../components/MateriaBadge";
 
 const MateriaDetalle = () => {
   // TODO: Mostrar el loading
   const { materia, loading } = useMateria()
-  console.log(materia)
 
   return (
     <section className="container-section">
-      <div className="container-materia">
-        <header className="section">
+      <header className="materia-header section">
+        <div className="materia-header-info">
           <h3 className="materia-title">
             {materia.nombre} <strong>({materia.id})</strong>
           </h3>
@@ -23,63 +22,50 @@ const MateriaDetalle = () => {
               <IconStar size={14}/>
               Promocionable
             </p>
-            <button className="materia-whatsapp-btn">
+            <a className="materia-whatsapp-btn">
               <IconBrandWhatsapp size={15}/>
               Whatsapp
-            </button>
+            </a>
           </div>
-        </header>
-        <main>
-          <section className="section">
-            <h3 className="section-title">
-              Mi estado
-            </h3>
-            <div className="materia-estados-container">
-              <button className="button-cursando">
-                Cursando
-              </button>
-              <button className="button-regularizada">
-                Regularizada
-              </button>
-              <button className="button-aprobada">
-                Aprobada
-              </button>
-              <button className="button-promocionada">
-                Promocionada
-              </button>
-            </div>
-          </section>
-          <section className="section">
-            <h3 className="section-title">
-              Correlativas
-            </h3>
-            <ul className="materia-correlativas-container">
-              {
-                materia.correlativas
-                ? materia.correlativas.map(correlativa => {
-                    return <MateriaBadge materia={correlativa}/>
-                  })
-                : ''
-              }
-            </ul>
-          </section>
-          <section className="section">
-            <h3 className="section-title">
-              Profesores
-            </h3>
-          </section>
-          <section className="section">
-            <h3 className="section-title">
-              Comisiones
-            </h3>
-          </section>
-          <section className="section">
-            <h3 className="section-title">
-              Carreras
-            </h3>
-          </section>
-        </main>
-      </div>
+        </div>
+        <aside className="materia-header-estado">
+          <button className="materia-button-estado">
+            <IconCirclePlus size={16}/>
+            Indicar mi estado
+          </button>
+        </aside>
+      </header>
+      <main>
+        <section className="section">
+          <h3 className="section-title">
+            Correlativas
+          </h3>
+          <ul className="materia-correlativas-container">
+            {
+              materia.correlativas
+              ? materia.correlativas.map(correlativa => {
+                  return <MateriaBadge materia={correlativa}/>
+                })
+              : ''
+            }
+          </ul>
+        </section>
+        <section className="section">
+          <h3 className="section-title">
+            Profesores
+          </h3>
+        </section>
+        <section className="section">
+          <h3 className="section-title">
+            Comisiones
+          </h3>
+        </section>
+        <section className="section">
+          <h3 className="section-title">
+            Carreras
+          </h3>
+        </section>
+      </main>
     </section>
   );
 };
