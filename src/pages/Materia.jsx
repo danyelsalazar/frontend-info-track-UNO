5aa5d03737bfb122b339fe74bb41fba801ac49ad
Ezagu <agustincalpedm@gmail.com>
2026-05-31 03:04:16 -0300

    separando materia en secciones

--- src/pages/Materia.jsx
+++ src/pages/Materia.jsx
@@ -5,37 +5,45 @@ const MateriaDetalle = () => {
   const { materia, loading } = useMateria()
 
   return (
-    <>
-      <section className="container-section-materia-detalle">
-        <div className="card container-materia-detalle">
-          <h3 className="title-name-materia-detalle">{materia.nombre}</h3>
-          <div className="container-info-materia-detalle-profes">
-            <p className="cuatri">
-              Cuatrimestre de dictado: {materia.semester}
-            </p>
-            <p className="cod-materia-detalle">
-              <b>Código</b> {materia.id}
-            </p>
-            <p className="title-profes-detalle">Profesores que dictan la materia:</p>
-            {materia.profesores?.map(profesor => (
-              <div key={profesor.id} className="info-mareria-detalle-profe">
-                <p className="profe-materia-detalle">
-                  <b>{'XX'}</b> {profesor.nombre}
-                </p>
-                <p className="email-profe-materia-detalle"> <b className="email-profe">Email: </b>{profesor.email}</p>
-              </div>
-            ))}
-
-            <ul className="list-coorrelativas">
-              <h5>Correlativas:</h5>
-              {materia.correlativas?.map(coorrelativa => (
-                <li key={coorrelativa.id}>{coorrelativa.nombre}</li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </section>
-    </>
+    <section className="container-section-materia-detalle">
+      <div className="container-materia-detalle">
+        <header>
+          <h3 className="title-name-materia-detalle">
+            <strong>
+              {materia.id}
+            </strong>
+            - {materia.nombre}
+          </h3>
+        </header>
+        <main>
+          <section className="section">
+            <h3 className="section-title">
+              Mi estado
+            </h3>
+          </section>
+          <section className="section">
+            <h3 className="section-title">
+              Correlativas
+            </h3>
+          </section>
+          <section className="section">
+            <h3 className="section-title">
+              Profesores
+            </h3>
+          </section>
+          <section className="section">
+            <h3 className="section-title">
+              Comisiones
+            </h3>
+          </section>
+          <section className="section">
+            <h3 className="section-title">
+              Carreras
+            </h3>
+          </section>
+        </main>
+      </div>
+    </section>
   );
 };
 
