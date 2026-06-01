5dbb709cf7ddf2d8b43f32c9f6cf3ddd830899fc
Ezagu <agustincalpedm@gmail.com>
2026-06-01 02:31:55 -0300

    descomponentizando pagina de materia y profesor

--- src/pages/Materia.jsx
+++ src/pages/Materia.jsx
@@ -3,6 +3,120 @@ import { useMateria } from "../hooks/useMateria"
 import { MateriaBadge } from "../components/MateriaBadge";
 import { Link } from "react-router-dom";
 
+const HeaderSection = ({materia}) => {
+  return (
+    <header className="materia-header section">
+      <div className="materia-header-top">
+        <h3 className="materia-title">
+          {materia.nombre} <strong>({materia.id})</strong>
+        </h3>
+        <button className="materia-button-estado">
+          <IconCirclePlus size={16}/>
+          Indicar estado
+        </button>
+      </div>
+      <BadgesContainer materia={materia}/>
+    </header>
+  )
+}
+
+const CorrelativasSection = ({correlativas}) => {
+  return (
+    <section className="section">
+      <h3 className="section-title">
+        <IconLink size={16}/>
+        Correlativas
+      </h3>
+      <ul className="materia-correlativas-container">
+        {
+          correlativas.length > 0
+          ? correlativas.map(correlativa => <MateriaBadge materia={correlativa} key={correlativa.id}/>)
+          : <p className="section-text"> Esta materia no tiene correlativas </p>
+        }
+      </ul>
+    </section>
+  )
+}
+
+const ProfesoresSection = ({profesores}) => {
+  return (
+    <section className="section">
+      <h3 className="section-title">
+        <IconUser size={16}/>
+        Profesores
+      </h3>
+      <ul className="materia-profesores-container">
+        {
+          profesores.length > 0 
+          ? profesores.map(profesor => <ProfesorCard profesor={profesor} key={profesor.id}/>)
+          : <p className="section-text">No hay información sobre profesores</p>
+        }
+      </ul>
+    </section>
+  )
+}
+
+const ComisionesSection = () => {
+  return (
+    <section className="section">
+      <h3 className="section-title">
+        <IconCalendarWeek size={16}/>
+        Comisiones
+      </h3>
+    </section>
+  )
+}
+
+const CarrerasSection = () => {
+  return (
+    <section className="section">
+      <h3 className="section-title">
+        <IconBuilding size={16}/>
+        Carreras
+      </h3>
+    </section>
+  )
+} 
+
+const BadgesContainer = ({materia}) => {
+  return (
+    <div className="materia-badge-container">
+      <p className="badge">
+        <IconCalendarCheck size={14}/>
+        {
+          materia?.cuatrimestreDictado.length === 2
+            ? "1º y 2º cuatrimestre"
+            : `${materia.cuatrimestreDictado[0]}º cuatrimestre`             
+        }
+        
+      </p>
+      {
+        materia.promocionable 
+        && (
+          <p className="badge">
+            <IconStar size={14}/>
+            Promocionable
+          </p>
+        )
+      }
+      {
+        materia.linkWhatsapp 
+        && (
+          <a 
+            id="whatsapp-btn"
+            className="badge"
+            href={materia.linkWhatsapp}
+            target="noblank"
+          >
+            <IconBrandWhatsapp size={15}/>
+            Whatsapp
+          </a>
+        )
+      }
+    </div>
+  )
+}
+
 const ProfesorCard = ({profesor}) => {
   return (
     <Link 
@@ -24,24 +138,7 @@ const ProfesorCard = ({profesor}) => {
   )
 }
 
-const NoCorrelativas = () => {
-  return (
-    <p className="section-text">
-      Esta materia no tiene correlativas
-    </p>
-  )
-}
-
-const NoProfesores = () => {
-  return (
-    <p className="section-text">
-      No hay información sobre profesores
-    </p>
-  )
-}
-
-const MateriaDetalle = () => {
-  // TODO: Mostrar el loading
+const Materia = () => {
   const { materia, loading } = useMateria()
 
   if(loading) {
@@ -58,93 +155,15 @@ const MateriaDetalle = () => {
 
   return (
     <section className="container-section">
-      <header className="materia-header section">
-        <div className="materia-header-top">
-          <h3 className="materia-title">
-            {materia.nombre} <strong>({materia.id})</strong>
-          </h3>
-          <button className="materia-button-estado">
-            <IconCirclePlus size={16}/>
-            Indicar estado
-          </button>
-        </div>
-        <div className="materia-badge-container">
-          <p className="badge">
-            <IconCalendarCheck size={14}/>
-            {
-              materia?.cuatrimestreDictado.length === 2
-                ? "1º y 2º cuatrimestre"
-                : `${materia.cuatrimestreDictado[0]}º cuatrimestre`             
-            }
-            
-          </p>
-          {
-            materia.promocionable 
-            && (
-              <p className="badge">
-                <IconStar size={14}/>
-                Promocionable
-              </p>
-            )
-          }
-          {
-            materia.linkWhatsapp 
-            && (
-              <a 
-                id="whatsapp-btn"
-                className="badge"
-                href={materia.linkWhatsapp}
-                target="noblank"
-              >
-                <IconBrandWhatsapp size={15}/>
-                Whatsapp
-              </a>
-            )
-          }
-        </div>
-      </header>
+      <HeaderSection materia={materia}/>
       <main>
-        <section className="section">
-          <h3 className="section-title">
-            <IconLink size={16}/>
-            Correlativas
-          </h3>
-          <ul className="materia-correlativas-container">
-            {
-              materia.correlativas.length > 0
-              ? materia.correlativas.map(correlativa => <MateriaBadge materia={correlativa}/>)
-              : <NoCorrelativas />
-            }
-          </ul>
-        </section>
-        <section className="section">
-          <h3 className="section-title">
-            <IconUser size={16}/>
-            Profesores
-          </h3>
-          <ul className="materia-profesores-container">
-            {
-              materia.profesores.length > 0 
-              ? materia?.profesores.map(profesor => <ProfesorCard profesor={profesor} key={profesor.id}/>)
-              : <NoProfesores />
-            }
-          </ul>
-        </section>
-        <section className="section">
-          <h3 className="section-title">
-            <IconCalendarWeek size={16}/>
-            Comisiones
-          </h3>
-        </section>
-        <section className="section">
-          <h3 className="section-title">
-            <IconBuilding size={16}/>
-            Carreras
-          </h3>
-        </section>
+        <CorrelativasSection correlativas={materia.correlativas}/>
+        <ProfesoresSection profesores={materia.profesores}/>
+        <ComisionesSection />
+        <CarrerasSection />
       </main>
     </section>
   );
 };
 
-export default MateriaDetalle;
+export default Materia;
--- src/pages/Profesor.jsx
+++ src/pages/Profesor.jsx
@@ -1,7 +1,85 @@
-import { Link, useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { useProfesor } from "../hooks/useProfesor"
 import { Reseña } from "../components/Reseña"
 import { Rating } from "@mui/material"
+import { MateriaBadge } from "../components/MateriaBadge"
+
+const HeaderSection = ({profesor}) => {
+  return (
+    <header className="profesor-container-header section">
+      <div className="profesor-header-logo">
+        {profesor.siglas}
+      </div>
+      <div className="profesor-header-info">
+        <h2 className="profesor-nombre">{profesor.apellido}, {profesor.nombre}</h2>
+        <p className="profesor-email">{profesor.email}</p>
+      </div>
+      <div className="profesor-rating-container">
+        <h2 className="profesor-rating-promedio">
+          {profesor?.promedioPuntuaciones || 0}/5
+        </h2>
+        <Rating 
+          size="small" 
+          readOnly 
+          defaultValue={profesor?.promedioPuntuaciones || 0}
+        />
+        <p className="profesor-rating-cantidad">
+          {`${profesor?.cantidadPuntuaciones} ${profesor?.cantidadPuntuaciones === 1 ? 'reseña' : 'reseñas'}`}
+        </p>
+      </div>
+    </header>
+  )
+}
+
+const MateriasSection = ({materias}) => {
+  return (
+    <section className="section">
+      <h3 className="section-title">
+        Materias que dicta
+      </h3>
+      <ul className="profesor-materias-container">
+        {
+          materias.map(materia => <MateriaBadge materia={materia} key={materia.id}/>)
+        }
+      </ul>
+    </section>
+  )
+}
+
+const MiReseñaSection = ({puntuaciones}) => {
+  return (
+    <section className="section">
+      <h3 className="section-title">
+        Mi reseña
+      </h3>
+      <div className="mi-reseña-section">
+        <p className="section-text">Todavía no dejaste una reseña para este profesor</p>
+        <button className="boton-mi-reseña">
+          + Dejar reseña
+        </button>
+      </div>
+    </section>
+  )
+}
+
+const ReseñasSection = ({puntuaciones}) => {
+  return (
+    <section className="section">
+      <h3 className="section-title">
+        Reseñas
+      </h3>
+      <ul className="profesor-reseñas-container">
+        {
+          puntuaciones.length === 0
+            ? <p>Todavía nadie dejó una reseña</p>
+            : puntuaciones.map(puntuacion => (
+            <Reseña puntuacion={puntuacion}/>
+            )) 
+        }
+      </ul>
+    </section>
+  )
+}
 
 export const Profesor = () => {
   const { profesor, loading } = useProfesor()
@@ -21,70 +99,10 @@ export const Profesor = () => {
         </button>
       </header>
       <div className="container-section">
-        <header className="profesor-container-header section">
-          <div className="profesor-header-logo">
-            {profesor.siglas}
-          </div>
-          <div className="profesor-header-info">
-            <h2 className="profesor-nombre">{profesor.apellido}, {profesor.nombre}</h2>
-            <p className="profesor-email">{profesor.email}</p>
-          </div>
-          <div className="profesor-rating-container">
-            <h2 className="profesor-rating-promedio">
-              {profesor?.promedioPuntuaciones || 0}/5
-            </h2>
-            <Rating 
-              size="small" 
-              readOnly 
-              defaultValue={profesor?.promedioPuntuaciones || 0}
-            />
-            <p className="profesor-rating-cantidad">
-              {`${profesor?.cantidadPuntuaciones} ${profesor?.cantidadPuntuaciones === 1 ? 'reseña' : 'reseñas'}`}
-            </p>
-          </div>
-        </header>
-        <section className="section">
-          <h3 className="section-title">
-            Materias que dicta
-          </h3>
-          <ul className="profesor-materias-container">
-            {
-              profesor.materias.map(materia => (
-                <Link 
-                  to={`/materia/${materia.id}`} className="materia-badge"
-                  key={materia.id}
-                >
-                  {materia.nombre}
-                </Link>
-              ))
-            }
-          </ul>
-        </section>
-        <section className="section">
-          <h3 className="section-title">
-            Mi reseña
-          </h3>
-          <div className="mi-reseña-section">
-            <p className="section-text">Todavía no dejaste una reseña para este profesor</p>
-            <button className="boton-mi-reseña">
-              + Dejar reseña
-            </button>
-          </div>
-        </section>
-        <section className="section">
-          <h3 className="section-title">
-            Reseñas
-          </h3>
-          <ul className="profesor-reseñas-container">
-            {
-              profesor.puntuaciones.length === 0
-                ? <p>Todavía nadie dejó una reseña</p>
-                : profesor.puntuaciones.map(puntuacion => (
-                <Reseña puntuacion={puntuacion}/>
-                )) 
-            }
-          </ul>
-        </section>
+        <HeaderSection profesor={profesor}/>
+        <MateriasSection materias={profesor.materias}/>
+        <MiReseñaSection puntuaciones={profesor.puntuaciones}/>
+        <ReseñasSection puntuaciones={profesor.puntuaciones}/>
       </div>
     </section>
   )
