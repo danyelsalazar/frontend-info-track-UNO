f4548c471131a085582fa2e1cae6d9ddf128b055
danyel salazar <danygasia2@gmail.com>
2026-06-07 16:38:27 -0300

    feature/botones de edicion y agregado de materias

--- src/pages/MateriasUser.jsx
+++ src/pages/MateriasUser.jsx
@@ -35,9 +35,7 @@ const MateriasUser = () => {
         ? p.carrera.id === filtroCarrera
         : true;
 
-      const coincideAnio = filtroAnio
-        ? p.anio === Number(filtroAnio)
-        : true;
+      const coincideAnio = filtroAnio ? p.anio === Number(filtroAnio) : true;
 
       return coincideCarrera && coincideAnio;
     });
@@ -52,15 +50,13 @@ const MateriasUser = () => {
   */
   const anios = [
     ...new Set(
-      materias.flatMap((m) =>
-        m.materia?.planEstudio?.map((p) => p.anio) || []
-      )
+      materias.flatMap((m) => m.materia?.planEstudio?.map((p) => p.anio) || []),
     ),
   ];
 
   return (
     <div className="materias-user-container">
-      <Header/>
+      <Header />
       <h2>Mis Materias</h2>
 
       <div className="filter-materias-user">
@@ -110,12 +106,34 @@ const MateriasUser = () => {
             */}
             {m.materia.planEstudio.map((p, index) => (
               <p key={index}>
-                {p.carrera.abreviacion} | {p.anio}° Año | {p.cuatrimestre}° cuatrimestre
+                {p.carrera.abreviacion} | {p.anio}° Año | {p.cuatrimestre}°
+                cuatrimestre
               </p>
             ))}
+            <div className="container-edit-materia-user">
+              <button className="edit-materia-user">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  viewBox="0 0 24 24"
+                >
+                  <g fill="none">
+                    <path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+                    <path
+                      fill="#d62c5b"
+                      d="M13 3a1 1 0 0 1 .117 1.993L13 5H5v14h14v-8a1 1 0 0 1 1.993-.117L21 11v8a2 2 0 0 1-1.85 1.995L19 21H5a2 2 0 0 1-1.995-1.85L3 19V5a2 2 0 0 1 1.85-1.995L5 3zm6.243.343a1 1 0 0 1 1.497 1.32l-.083.095l-9.9 9.899a1 1 0 0 1-1.497-1.32l.083-.094z"
+                    />
+                  </g>
+                </svg>
+              </button>
+            </div>
           </div>
         ))
       )}
+
+      <div className="container-button-add-materia"></div>
+      <button className="add-materia">Agregar materias</button>
     </div>
   );
 };
